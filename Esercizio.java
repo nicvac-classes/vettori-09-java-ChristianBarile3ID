b0edc662262d58e4db3616590fe192dd01919042
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2024-11-27 16:38:38 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,25 +1,60 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
 
-// Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
+
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
-
-        //Creo l'oggetto in per l'input da tastiera
         Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        int giri, i, iMin;
+        float sommaHamilton, sommaVerstappen, veloceHamilton, veloceVerstappen;
+        do {
+            System.out.println("Da quanti giri è composta la gara?");
+            giri = in.nextInt();
+        } while (giri<3);
+        float [] tempiHamilton = new float [giri];
+        float [] tempiVerstappen = new float [giri];
+        sommaHamilton = 0;
+        sommaVerstappen = 0;
+        i = 0;
+        while (i<tempiHamilton.length) {
+            System.out.println("In quanto tempo ha Hamilton completato il giro n°" + i+1 + "?");
+            tempiHamilton[i] = in.nextFloat();
+            sommaHamilton += tempiHamilton[i];
+            System.out.println("In quanto tempo ha Verstappen completato il giro n°" + i+1 + "?");
+            tempiVerstappen[i] = in.nextFloat();
+            sommaVerstappen += tempiVerstappen[i];
+            i++;
+        }
+        if (sommaHamilton<sommaVerstappen) {
+            System.out.println("Il vincitore della gara è Hamilton, che ha finito la gara in " + sommaHamilton);
+        } else {
+            System.out.println("Il vincitore della gara è Verstappen, che ha finito la gara in " + sommaVerstappen);
+        }
+        i = 0;
+        iMin = 0;
+        while (i<tempiHamilton.length) {
+            if (tempiHamilton[i]<tempiHamilton[iMin]) {
+                iMin = i;
+            }
+            i++;
+        }
+        veloceHamilton = tempiHamilton[iMin];
+        System.out.println("Il giro più veloce di Hamilton è stato il giro n°" + iMin+1 + " ed ha totalizzato un tempo di " + veloceHamilton);
+        i = 0;
+        iMin = 0;
+        while (i<tempiVerstappen.length) {
+            if (tempiVerstappen[i]<tempiVerstappen[iMin]) {
+                iMin = i;
+            }
+            i++;
+        }
+        veloceVerstappen = tempiVerstappen[iMin];
+        System.out.println("Il giro più veloce di Verstappen è stato il giro n°" + iMin+1 + " ed ha totalizzato un tempo di " + veloceVerstappen);
+        if (veloceVerstappen<veloceHamilton) {
+            System.out.println("Il giro più veloce è stato quello di Verstappen");
+        } else {
+            System.out.println("Il giro più veloce è stato quello di Hamilton");
+        }
     }
 }
 
